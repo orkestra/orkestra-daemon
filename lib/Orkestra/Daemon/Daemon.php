6f0c3142b834e8afccb34d3c51aa94287ce7d133
Tyler Sommer <sommertm@gmail.com>
2012-12-28 17:53:42 -0800

    Refactored execute method on Daemon, added unit test for Daemon.

--- lib/Orkestra/Daemon/Daemon.php
+++ lib/Orkestra/Daemon/Daemon.php
@@ -28,17 +28,35 @@ class Daemon
     protected $maxChildren = 1;
 
     /**
+     * @var int Number of milliseconds to sleep
+     */
+    protected $sleepTime = 1000;
+
+    /**
      * Constructor
      *
      * @throws \RuntimeException if the PCNTL or POSIX extensions are not loaded
      */
     public function __construct()
     {
-        if (!function_exists('pcntl_fork')) {
+        if (!extension_loaded('pcntl')) {
             throw new \RuntimeException('The Daemon class relies on the PCNTL extension, which is not available on your PHP installation.');
-        } elseif (!function_exists('posix_kill')) {
+        } elseif (!extension_loaded('posix')) {
             throw new \RuntimeException('The Daemon class relies on the POSIX extension, which is not available on your PHP installation.');
         }
+    }
+
+    /**
+     * Adds a worker
+     *
+     * A worker must be a valid executable
+     *
+     * @param string $worker    Path to 'work'
+     * @param array  $arguments
+     */
+    public function addWorker($worker, $arguments = array())
+    {
+        $this->workers[] = array($worker, (array) $arguments);
     }
 
     /**
@@ -72,40 +90,28 @@ class Daemon
 
         declare(ticks = 1);
         pcntl_signal(SIGTERM, array($this, 'handleSignal'));
-        pcntl_signal(SIGHUP, array($this, 'handleSignal'));
-        pcntl_signal(SIGINT, array($this, 'handleSignal'));
+        pcntl_signal(SIGHUP,  array($this, 'handleSignal'));
+        pcntl_signal(SIGINT,  array($this, 'handleSignal'));
         pcntl_signal(SIGUSR1, array($this, 'handleSignal'));
+        pcntl_signal(SIGUSR2, array($this, 'handleSignal'));
 
         do {
             // Spawn a new worker if necessary
             if ($this->hasMoreWork() && count($this->pids) < $this->maxChildren) {
-                $pid = pcntl_fork();
-                list($worker, $arguments) = $this->getNextWorker();
-
-                if (!$pid) {
-                    // New worker process
-                    pcntl_exec($worker, $arguments);
-                } else {
-                    $this->pids[] = $pid;
-                }
+                $this->spawnWorker();
             }
 
-            // Clean up any workers that have exited on their own
-            do {
-                $pid = pcntl_waitpid(-1, $status, WNOHANG);
-
-                if ($pid <= 0)
-                    break;
-
-                unset($this->pids[array_search($pid, $this->pids)]);
-            } while (true);
+            $this->cleanUpFinishedWorkers();
 
             // Exit the daemon if no more work is needed
             if (count($this->workers) <= 0 && count($this->pids) <= 0) {
-                $this->handleSignal(SIGTERM);
+                $this->terminate(SIGTERM);
+
+                // Necessary because the unit test overrides exit
+                return;
             }
 
-            sleep(1);
+            usleep($this->sleepTime);
         } while (true);
     }
 
@@ -120,19 +126,44 @@ class Daemon
             case SIGTERM:
             case SIGHUP:
             case SIGINT:
-                foreach ($this->pids as $pid) {
-                    posix_kill($pid, $signal);
-                }
+                $this->terminate($signal);
 
-                foreach ($this->pids as $pid) {
-                    pcntl_waitpid($pid, $status);
-                }
-
-                exit;
+                break;
             case SIGUSR1:
-                // echo some kind of status
-                printf('I have %s child processes', count($this->pids));
+            case SIGUSR2:
+                $this->handleUserDefinedSignal($signal);
         }
+    }
+
+    /**
+     * Terminates the Daemon
+     *
+     * This method should perform any cleanup, killing all child processes
+     *
+     * @param int $signal
+     */
+    protected function terminate($signal)
+    {
+        foreach ($this->pids as $pid) {
+            posix_kill($pid, $signal);
+        }
+
+        foreach ($this->pids as $pid) {
+            pcntl_waitpid($pid, $status);
+        }
+
+        exit;
+    }
+
+    /**
+     * Handles user defined signals
+     *
+     * This method is called when SIGUSR1 or SIGUSR2 are sent to the Daemon
+     *
+     * @param int $signal
+     */
+    protected function handleUserDefinedSignal($signal)
+    {
     }
 
     /**
@@ -156,6 +187,42 @@ class Daemon
     }
 
     /**
+     * Spawns a new worker process
+     *
+     * @return void
+     */
+    protected function spawnWorker()
+    {
+        $pid = pcntl_fork();
+        list($worker, $arguments) = $this->getNextWorker();
+
+        if (!$pid) {
+            // New worker process
+            pcntl_exec($worker, $arguments);
+        } else {
+            $this->pids[] = $pid;
+        }
+    }
+    
+    /**
+     * Cleans up any workers that have exited on their own
+     *
+     * @return void
+     */
+    protected function cleanUpFinishedWorkers()
+    {
+        do {
+            $status = 0;
+            $pid = pcntl_waitpid(-1, $status, WNOHANG);
+
+            if ($pid <= 0)
+                break;
+
+            unset($this->pids[array_search($pid, $this->pids)]);
+        } while (true);
+    }
+
+    /**
      * Gets the parent's PID
      *
      * @return int
@@ -166,19 +233,6 @@ class Daemon
     }
 
     /**
-     * Adds a worker
-     *
-     * A worker must be a valid executable
-     *
-     * @param string $worker    Path to 'work'
-     * @param array  $arguments
-     */
-    public function addWorker($worker, $arguments = array())
-    {
-        $this->workers[] = array($worker, (array) $arguments);
-    }
-
-    /**
      * Sets the maximum number of children the daemon can spawn at any given time
      *
      * @param int $max
@@ -186,5 +240,35 @@ class Daemon
     public function setMaxChildren($max)
     {
         $this->maxChildren = (int) $max;
+    }
+
+    /**
+     * Sets the number of milliseconds the daemon will sleep between loops
+     *
+     * @param int $sleepTime
+     */
+    public function setSleepTime($sleepTime)
+    {
+        $this->sleepTime = (int) $sleepTime;
+    }
+
+    /**
+     * Gets the maximum number of children the daemon can spawn at any given time
+     *
+     * @return int
+     */
+    public function getMaxChildren()
+    {
+        return $this->maxChildren;
+    }
+
+    /**
+     * Gets the number of milliseconds the daemon will sleep between loops
+     *
+     * @return int
+     */
+    public function getSleepTime()
+    {
+        return $this->sleepTime;
     }
 }
