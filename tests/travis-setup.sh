4621e8c1a0b8162dd39a3e71851726a9cad644cf
Tyler Sommer <sommertm@gmail.com>
2012-12-31 13:57:13 -0800

    Added missing runkit configuration in travis build config

--- tests/travis-setup.sh
+++ tests/travis-setup.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/zenovich/runkit.git
 sh -c "cd runkit && phpize && ./configure && make && sudo make install"
-echo "extension=runkit.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+echo "extension=runkit.so\nrunkit.internal_override=1" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 
 git clone https://github.com/sebastianbergmann/php-test-helpers.git
 sh -c "cd php-test-helpers && phpize && ./configure && make && sudo make install"
