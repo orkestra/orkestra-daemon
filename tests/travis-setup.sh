c2f90f2b6c49865da7dee0ccffdb397055bdb91a
Tyler Sommer <sommertm@gmail.com>
2012-12-31 13:25:26 -0800

    Added travis configuration and initialization script

---
+++ tests/travis-setup.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/zenovich/runkit.git
+sh -c "cd runkit && phpize && ./configure && make && sudo make install"
+echo "extension=runkit.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+
+git clone https://github.com/sebastianbergmann/php-test-helpers.git
+sh -c "cd php-test-helpers && phpize && ./configure && make && sudo make install"
+echo "zend_extension=test_helpers.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+
+composer install
