69e281077cd779d6c0c932064b45cd1a3ed04a5b
Tyler Sommer <sommertm@gmail.com>
2012-12-31 13:52:41 -0800

    Travis build configuration puts test_helpers extension loading in a separate file to ensure it is loaded last

--- tests/travis-setup.sh
+++ tests/travis-setup.sh
@@ -4,6 +4,6 @@ echo "extension=runkit.so" >> `php --ini | grep "Loaded Configuration" | sed -e 
 
 git clone https://github.com/sebastianbergmann/php-test-helpers.git
 sh -c "cd php-test-helpers && phpize && ./configure && make && sudo make install"
-echo "zend_extension=`php -r "echo ini_get('extension_dir');"`/test_helpers.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+echo "zend_extension=`php -r "echo ini_get('extension_dir');"`/test_helpers.so" >> `php --ini | grep "Scan for additional .ini files in:" | sed -e "s|.*:\s*||"`/z_test_helpers.ini
 
 composer install
